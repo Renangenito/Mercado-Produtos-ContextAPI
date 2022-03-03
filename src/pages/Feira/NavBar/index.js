9266b511697123867937e7734a2255404d906525
renan lima <renangenito@hotmail.com>
2022-03-03 15:17:42 -0300

    Salvando o projeto

--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -3,8 +3,9 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
-
 export default function NavBar() {
+  
+
   return (
     <Nav>
       <Logo />
